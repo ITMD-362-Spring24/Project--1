55336de7d5776377635e64fe64eb8c6c705044b6
Allen Domalik <allenDomalik@yahoo.com>
2024-02-24 16:52:40 -0600

    alot of adding, testing and resizing

--- js/interact.js
+++ js/interact.js
@@ -1,12 +1,12 @@
 function upDate(previewPic){
   var src=previewPic.getAttribute("src");
-  var alt=previewPic.getAttribute("alt");
-  document,getElementById('image').innerHTML=alt;
-  
-  document.getElementById('image').style.backgroundImage="url(" + src + " )";
+   var alt=previewPic.getAttribute("alt");
+  document.getElementById('image').innerHTML=alt;
+  document.getElementById('image').style.backgroundImage="url(" +  src  +  "  )";
 }
 
-function unDo(){
-   document.getElementById('image').style.backgroundImage="none";
-   document.getElementById('image').innerHTML="Hover over an image';
+   function unDo(){
+  document.getElementById('image').style.backgroundImage="none";
+   document.getElementById('image').innerHTML="Hover over an image";
 }
+
